e8b999fa3a75cc6731b5f846cdf17c1f5ffeef83
Ibrahim <ibrahim.13mohamed@gmail.com>
2022-03-29 12:35:34 -0500

    quick eats project

---
+++ server/server.js
@@ -0,0 +1,70 @@
+
+// nodejs libraries in use
+
+
+const express = require ('express'); //use express
+const path = require('path')
+
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') }   )
+
+const bodyParser = require('body-parser');
+const yelp = require('yelp-fusion');
+const app = express();
+
+
+const API_YELP_KEY = process.env.API_YELP_KEY
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false }) 
+
+app.set('views', path.join(__dirname, '../public/views'))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, '../public')))
+
+const client = yelp.client(API_YELP_KEY);
+
+//get request renders the home page using ejs  
+app.get('/', function(req, res) { 
+    res.render('home')
+  })
+
+
+  app.post('/results', urlencodedParser, function(req, res) {
+    formData = req.body
+
+    client.search(formData).then(response => {
+        let randomNum = Math.floor(Math.random() * 20)
+
+        const randomResult = response.jsonBody.businesses[randomNum];
+        const placeLocation = randomResult.location.display_address
+        const placeName = randomResult.name
+        const placeRating = randomResult.rating
+        const img = randomResult.image_url
+        const link = randomResult.url
+        const phoneNumber = randomResult.display_phone
+        
+        let resturantData = {
+            name: placeName, 
+            rating: placeRating, 
+            location: placeLocation, 
+            image: img, 
+            phone: phoneNumber,
+            onlineLink: link, 
+            
+        }
+
+
+        res.render('result', resturantData)
+       
+      }).catch(e => {
+        console.log(e);
+      })
+  
+    
+});
+
+
+const port = process.env.port || 4400
+
+app.listen(port, () => {
+    console.log(`server running on ${port}`)
+}); 
