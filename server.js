434fac7def197c3e3a71aa2c7983f529d3535c5e
Ibrahim <ibrahim.13mohamed@gmail.com>
2022-03-29 13:53:19 -0500

    quick eats project

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const client = yelp.client(API_YELP_KEY);
 
 //get request renders the home page using ejs  
 app.get('/', function(req, res) { 
-    res.sendFile(path.join(__dirname, '/public/views'))
+    res.sendFile(path.join(__dirname, '/public/views/home'))
     res.render('home')
     
   })
